103c12f3f68f0ceaa15c70213ee4c1fd4989ad4c
Hatim Lahwaouir <hlahwaou@e1r10p15.1337.ma>
2023-11-25 09:46:00 +0100

    Organizer done !

--- Organizer.py
+++ Organizer.py
@@ -1,5 +1,4 @@
 import os
-from pathlib import Path
 import OrganizerExceptions
 import re
 
@@ -9,6 +8,12 @@ class Organizer:
         "The constructor's purpose is to parse the 'Organizer' file and set all the attributes I need."
     '''
     def __init__(self, file) -> None:
+        self.header = ''' 
+        |\\         /|   _________                    __               _________
+        |  \\      / |       |         |            /    \\    | /          |
+        |   \\    /  |       |         |           |      |   |/           |
+        |    \\  /   |       |         |            \\    /    |\\           |
+        |     \\/    |   ---------     -------        --      | \\      ---------\n''' 
     # bool to check that every thing went good 
         self._good = True
     # searching attributes
@@ -21,11 +26,17 @@ class Organizer:
         self._dirsToOrganize01 = []
         self._exts = []
         self._NewDirName = []
+        try:
+            self.results = open("results.txt", "w")
+        except:
+            print("Couldn't open the results.txt file")
+            self._good = False
+            exit(1)
         # we are parsing the file and calling on eache task it's function to parse it  
+        self.results.write(self.header)
         with open(file, 'r') as file:
             task = ["S:", "O:"]
             pars = [self.ParsSearching, self.ParsOrganizer]
-            
             for line in file:
                 tokens = line.split()
                 if len(tokens) == 0:
@@ -34,27 +45,30 @@ class Organizer:
                 try:
                     idx = task.index(tokens[0])
                 except:
-                    print("Using of anknown task")
+                    self.results.write("Using of anknown task in the conf file.\n")
                     self._good = False
                     exit(0)
                 # we are checking if the parssing throw an exception
                 try:
                     pars[idx](tokens)
                 except OrganizerExceptions.Parssing:
-                    print("An error occurred while parsing the task {}".format(task[idx]))
+                    self.results.write("An error occurred while parsing the task {}\n".format(task[idx]))
                     self._good = False
                     exit(1)
                 except OrganizerExceptions.FileDoesntExist:
-                    print("A directory or File in the task {} dosen't exist.".format(task[idx]))
+                    self.results.write("A directory or File in the task {} dosen't exist.\n".format(task[idx]))
                     self._good = False
                     exit(1)
                 except OrganizerExceptions.PathIsNotDir:
-                    print("A Path given in the task {} excpected to be a dirictory but It's a file.".format(task[idx]))
+                    self.results.write("A Path given in the task {} excpected to be a dirictory but It's a file.\n".format(task[idx]))
                     self._good = False
                     exit(1)
                 except OrganizerExceptions.PermissionDenied:
-                    print("Task {} was attempting to perform an action without having the necessary permission.".format(task[idx]))
+                    self.results.write("Task {} was attempting to perform an action without having the necessary permission.\n".format(task[idx]))
+                    self._good = False
                     exit(1)
+
+
 
     # *********************** The parsser of the Organizer task *********************** 
     def ParsOrganizer(self, tokens):
@@ -65,7 +79,7 @@ class Organizer:
                 raise OrganizerExceptions.FileDoesntExist
             if (not os.path.isdir(tokens[1])):
                 raise OrganizerExceptions.PathIsNotDir
-            self._dirsToOrganize.append(tokens[1])
+            self._dirsToOrganize00.append(tokens[1])
         elif len(tokens) == 4:
             # checking if the path given is a directory
             if (not os.path.exists(tokens[1])):
@@ -111,13 +125,13 @@ class Organizer:
             try:
                 os.chdir(dir)
             except:
-                print("We couldn't accesse this path '{}'.".format(dir))
+                self.results.write("We couldn't accesse this path '{}'.\n".format(dir))
             try:
                 os.mkdir(newDir)
             except FileExistsError:
                 pass
             except:
-                print("Couldn't creat the Directory '{}' in this Path '{}'.".format(newDir, dir))
+                self.results("Couldn't creat the Directory '{}' in this Path '{}'.\n".format(newDir, dir))
                 continue
             for file in os.listdir():
                 if os.path.isdir(file):
@@ -127,8 +141,7 @@ class Organizer:
                     continue
                 if token[1] in ext:
                     os.replace(os.path.join(dir, file), os.path.join(dir, os.path.join(newDir, file)))
-            
-            
+
     # *********************** The parsser of the Searching task *********************** 
     def ParsSearching(self, tokens):
         if len (tokens) < 2:
@@ -144,7 +157,7 @@ class Organizer:
                 raise OrganizerExceptions.FileDoesntExist
     # *********************** The Searching task *********************** 
     
-    def searching(self) -> None:
+    def searching(self):
         print("Searching !!\n" * 4, end="")
         # here we are searching
         
@@ -164,20 +177,13 @@ class Organizer:
 
     # ***************** the purpose of the Destructor is to write to the file result at the end ,It provides a  summury of what this python program does **************
     def __del__(self):
-        header = ''' 
-                        |\\         /|   _________                    __               _________
-                        |  \\      / |       |         |            /    \\    | /          |
-                        |   \\    /  |       |         |           |      |   |/           |
-                        |    \\  /   |       |         |            \\    /    |\\           |
-                        |     \\/    |   ---------     -------        --      | \\      ---------
-        ''' 
-
-        with open("results.txt", 'w') as results:
-            results.write(header)
-            # if (not self._good):
-            #     return
-            # for target,path in zip(self._targets, self._targetPath):
-            #     if (len(path)):
-            #         print("The {} was found their path is {}".format(target, path))
-            #     else:
-            #         print("The {} wasn't found".format(target))
+        if (not self._good):
+            print("Sorry the prgoram couldn't work as excpected")
+            self.results.close()
+            return
+        for target,path in zip(self._targets, self._targetPath):
+            if (len(path)):
+                self.results.write("The '{}' was found ,Their path is '{}'.\n".format(target, path))
+            else:
+                self.results.write("The '{}' wasn't found.\n".format(target))
+        self.results.close()
